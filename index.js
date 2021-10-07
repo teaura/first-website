0f781571c9f26d3ad6d93f6ac713b7e6a10a7927
teaura <91853621+teaura@users.noreply.github.com>
2021-10-07 12:51:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+"hello"+" "+"world"
+'hello world'
+function welcome() { alert("hello"+" "+"world");}
+undefined
+welcome();
+undefined
+function hello(Teaura) { var name; alert("hello"+Teaura); }
+undefined
+hello("Teaura");
+undefined
+
